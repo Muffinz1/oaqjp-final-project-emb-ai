b851e24092902d08502ca4326d47e9c43406f39e
Amir <125520413+Muffinz1@users.noreply.github.com>
2026-08-21 19:23:25 +0300

    Fix variable name typo in emotion_detector function

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,10 +1,12 @@
 import requests
 import json
 
-def emotion_detector(text_to_analyze):
+def emotion_detector(text_to_analyse):
     url = 'https://skills.network'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj = { "raw_document": { "text": text_to_analyze } }
+    
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    
     response = requests.post(url, json=myobj, headers=headers)
     formatted_response = json.loads(response.text)
     
@@ -15,6 +17,7 @@ def emotion_detector(text_to_analyze):
     joy_score = emotions['joy']
     sadness_score = emotions['sadness']
     dominant_emotion = max(emotions, key=emotions.get)
+    
     result = {
         'anger': anger_score,
         'disgust': disgust_score,
