622118a6bc06311e1d1369df2e6a573c6db7a30a
Amir <125520413+Muffinz1@users.noreply.github.com>
2026-08-21 19:41:43 +0300

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,7 +8,9 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     
     response = requests.post(url, json=myobj, headers=headers)
+    
     formatted_response = json.loads(response.text)
+    
     emotions = formatted_response['emotionPredictions'][0]['emotion']
     
     anger_score = emotions['anger']
