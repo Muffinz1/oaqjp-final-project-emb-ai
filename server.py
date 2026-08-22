f1b9ddefeeddd6ed197e0ab04e4b97deeb60d8a0
Amir <125520413+Muffinz1@users.noreply.github.com>
2026-08-22 06:05:36 +0300

    Fixing server.py importing
    Updated import statement for emotion_detector and added docstrings for functions.

--- server.py
+++ server.py
@@ -1,20 +1,29 @@
+"""
+Flask server configuration for The Emotion Detection application.
+"""
 from flask import Flask, render_template, request
-from EmotionDetection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    """
+    Analyzes request argument inputs
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-    
+
     response = emotion_detector(text_to_analyze)
-    
+
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
     joy = response['joy']
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
     
     return (
         f"For the given statement, the system response is 'anger': {anger}, "
@@ -24,7 +33,9 @@ def emotion_analyzer():
 
 @app.route("/")
 def render_index_page():
-
+    """
+    Renders the main template index page view interface.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
